a9fc49a4966b2c3f4c6530069a64658ac271b560
Terence21 <terencepeterson21@gmail.com>
2020-09-03 08:02:05 -0700

    created EquilateralTriangle class extending triangle classwith allSides members, constructor and getter and setter methods, while overriding printDimensions and getArea

---
+++ src/EquilateralTriangle.java
@@ -0,0 +1,24 @@
+public class EquilateralTriangle extends Triangle{
+
+    private double allSides;
+
+    public EquilateralTriangle(String name){
+        super(name);
+        this.allSides = 0;
+    }
+
+    public void setDimensions(double allSides){
+        this.allSides = allSides;
+    }
+
+    @Override
+    public void printDimensions(){
+        System.out.println("--Dimensions--\n\tSides Length: " + allSides +
+                "\n\tArea: " );
+    }
+
+    @Override
+    public double getArea(){
+        return (Math.sqrt(3) / 4) * (this.allSides * this.allSides);
+    }
+}
