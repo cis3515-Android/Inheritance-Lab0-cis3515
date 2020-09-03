e656e99a233ce2c26a7b032459c98a5a0c6f2a4f
Terence21 <terencepeterson21@gmail.com>
2020-09-03 09:11:50 -0700

    fixed modifiers on main class functions to private, finished all create methods for all shapes and works as desired

--- src/main.java
+++ src/main.java
@@ -2,31 +2,54 @@ import java.util.Scanner;
 
 public class main{
     public static void main(String args[]){
-
+        createShape();
+        createSquare();
+        createCircle();
+        createTriangle();
+        createEquilateralTriangle();
     }
 
-    public static void createShape(){
+    private static void createShape(){
         Shape shape = new Shape("Shape");
         shape.printDimensions();
     }
 
-    public static Square createSquare(){
+    private static void createSquare(){
         Square square = new Square("Square");
         String [] dimensions = {"Length", "Height"};
         double [] inputVals = getInput(2,"Square",dimensions);
         square.setDimensions(inputVals[0], inputVals[1]);
-    }
-
-    public static Circle createCircle(){
+        square.printDimensions();
 
     }
 
-    public static Triangle createTriangle(){
+    private static void createCircle(){
+        Circle circle = new Circle("Circle");
+        String [] dimensions = {"radius"};
+        double[] inputVals = getInput(1, "Circle", dimensions);
+        circle.setDimensions(inputVals[0]);
+        circle.printDimensions();
+    }
 
+    public static void createTriangle(){
+        Triangle triangle = new Triangle("Triangle");
+        String[] dimensions = {"SideA", "SideB", "SideC"};
+        double[] inputVals = getInput(3, "Triangle", dimensions);
+        triangle.setDimensions(inputVals[0], inputVals[1], inputVals[2]);
+        triangle.printDimensions();
+
+    }
+
+    public static void createEquilateralTriangle(){
+        EquilateralTriangle equilateralTriangle = new EquilateralTriangle("Equilateral Triangle");
+        String[] dimensions = {"AllSides"};
+        double[] inputVals = getInput(1,"Equilateral Triangle", dimensions);
+        equilateralTriangle.setDimensions(inputVals[0]);
+        equilateralTriangle.printDimensions();
     }
 
     private static void promptUser(int numOfInputs, String shapeName){
-        System.out.println("Enter " + numOfInputs + "to create a " + shapeName + "\nType '-1' to exit...\n\n");
+        System.out.println("\n\nEnter " + numOfInputs + " dimensions to create a " + shapeName + "\nType '-1' to exit...\n");
     }
 
     private static void promptInput(int counter, String[] dimension){
@@ -35,19 +58,29 @@ public class main{
 
     private static double[] getInput(int numOfInputs, String shapeName, String[] dimensions){
         promptUser(numOfInputs, shapeName);
-        double inputArr[] = new double[numOfInputs-1];
+        double inputArr[] = new double[numOfInputs];
         double input = 0;
-        int counter = 1;
+        int counter = 0;
         Scanner sc = new Scanner(System.in);
+
         do{
             promptInput(counter,dimensions);
             while(!sc.hasNextDouble()){
+                sc.next();
                 System.out.println("Please enter a valid double...\n\n");
-
+                promptInput(counter, dimensions);
             }
-           inputArr[counter] = sc.nextDouble();
+
+            input = sc.nextDouble();
+
+            if (input == -1){
+                System.out.println("GoodBye");
+                System.exit(1);
+            }
+
+           inputArr[counter] = input;
            counter++;
-        }while (counter < numOfInputs );
+        }while (counter < numOfInputs);
 
         return inputArr;
     }
